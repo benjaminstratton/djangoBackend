510d30b0598255215968cae1388a131008cbe975
bstrat <benstratton100@gmail.com>
2023-03-27 17:04:33 -0500

    connected serializer and model

--- backend_api/star_app/views.py
+++ backend_api/star_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import StarSerializer
+from .models import Star
 
-# Create your views here.
+class StarList(generics.ListCreateAPIView):
+    queryset = Star.objects.all().order_by('id')
+    serializer_class = StarSerializer
+
+class StarDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Star.objects.all().order_by('id')
+    serializer_class = StarSerializer
