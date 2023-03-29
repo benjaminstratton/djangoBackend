d4ae3b5a6352e01228c3c3a00df96b5e11bcab31
bstrat <benstratton100@gmail.com>
2023-03-29 11:30:11 -0500

    MVP backend complete

--- backend_api/backend_api/settings.py
+++ backend_api/backend_api/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'star_app',
     'corsheaders',
     'rest_framework',
+    'star_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend_api/star_app/urls.py
+++ backend_api/star_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('app', views.CrudList.as_view(), name='crud_list'),
-    path('app/<int:pk>', views.CrudDetail.as_view(), name='crud_detail')
+    path('experiences', views.CrudList.as_view(), name='crud_list'),
+    path('experiences/<int:pk>', views.CrudDetail.as_view(), name='crud_detail')
 ]
